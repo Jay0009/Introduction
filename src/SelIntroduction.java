9e65be9a76c9d87a2331ab0e96cd9b5d40f102ba
Jay <jay.leo009@gmail.com>
2024-05-11 17:02:53 -0400

    5. Invoke and run same program in Firefox Browser
    Go to https://github.com/mozilla/geckodriver/releases  and download
    geckodriver-v0.34.0-macos-aarch64.tar.gz for m1 chip mac

--- src/SelIntroduction.java
+++ src/SelIntroduction.java
@@ -1,5 +1,6 @@
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class SelIntroduction
 {
@@ -11,8 +12,14 @@ public class SelIntroduction
 //		Set properties without using Selenium manager on chrome and invoke chrome browser
 //		System.setProperty("webdriver.chome.driver", "/Users/jay/Desktop/VdemiCourse/driver/chrome-mac-arm64/Google Chrome for Testing.app");
 		
+//		Set properties for gecko driver
+		System.setProperty("webdriver.gecko.driver", "/Users/jay/Desktop/VdemiCourse/driver/geckodriver");
+		
 		//Invoking chrome browser
-		WebDriver driver = new ChromeDriver();
+//		WebDriver driver = new ChromeDriver();
+		
+		//Invoking Firefox Driver
+		WebDriver driver = new FirefoxDriver();
 		
 		//get title of the Web page
 		driver.get("https://rahulshettyacademy.com");
