a96573cba32d35c55e04c1385d05c80bd3499283
Jay <jay.leo009@gmail.com>
2024-05-11 18:20:58 -0400

    8. Implicit wait will trigger for max of given seconds when required.
    Lecture 35: In this case, as 5 seconds is given, it will wait for 5
    seconds if the driver  is not able to find the element for upto 5
    seconds on each steps before moving forward. If the element doesn't show
    till 5 seconds, it will fail the test. This is the use of implicit wait.

--- src/Locators.java
+++ src/Locators.java
@@ -1,3 +1,4 @@
+import java.time.Duration;
 import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.By;
@@ -7,11 +8,14 @@ import org.openqa.selenium.chrome.ChromeDriver;
 public class Locators
 {
 
-	public static void main(String[] args)
+	public static void main(String[] args) throws InterruptedException
 	{
 		//Invoke chrome browser
 		System.setProperty("webdriver.chome.driver", "/Users/jay/Desktop/VdemiCourse/driver/chrome-mac-arm64/Google Chrome for Testing.app");
 		WebDriver driver = new ChromeDriver();
+		
+		//Implicit wait to resolve the issue
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
 		
 		//go to webpage
 		driver.get("https://rahulshettyacademy.com/locatorspractice/");
@@ -31,6 +35,12 @@ public class Locators
 		//Get error message
 		String errorText = driver.findElement(By.cssSelector("p.error")).getText();
 		System.out.println(errorText);
+		
+		//Thread.sleep to check and see execution
+		Thread.sleep(5000);
+		
+		//Close driver
+		driver.close();
 		
 
 	}
