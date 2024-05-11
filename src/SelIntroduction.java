19efe223f117b124fde8b902d431d31f2e69a734
Jay <jay.leo009@gmail.com>
2024-05-11 17:18:27 -0400

    6. Invoke and run same program on Edge browser
    Go to https://developer.microsoft.com/en-us/microsoft-edge/tools/webdriver?form=MA13LH
    and download Mac M1 version of the corresponding edge version

--- src/SelIntroduction.java
+++ src/SelIntroduction.java
@@ -1,5 +1,6 @@
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class SelIntroduction
@@ -13,13 +14,19 @@ public class SelIntroduction
 //		System.setProperty("webdriver.chome.driver", "/Users/jay/Desktop/VdemiCourse/driver/chrome-mac-arm64/Google Chrome for Testing.app");
 		
 //		Set properties for gecko driver
-		System.setProperty("webdriver.gecko.driver", "/Users/jay/Desktop/VdemiCourse/driver/geckodriver");
+//		System.setProperty("webdriver.gecko.driver", "/Users/jay/Desktop/VdemiCourse/driver/geckodriver");
+		
+//		Set properties for edge browser
+		System.setProperty("webdriver.edge.driver", "/Users/jay/Desktop/VdemiCourse/driver/edgedriver_mac64_m1/msedgedriver");
 		
 		//Invoking chrome browser
 //		WebDriver driver = new ChromeDriver();
 		
 		//Invoking Firefox Driver
-		WebDriver driver = new FirefoxDriver();
+//		WebDriver driver = new FirefoxDriver();
+		
+		//Invoking Edge browser
+		WebDriver driver = new EdgeDriver();
 		
 		//get title of the Web page
 		driver.get("https://rahulshettyacademy.com");
