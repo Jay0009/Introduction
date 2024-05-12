5d3be1c371927abfab21fe945471778b25a98da5
Jay <jay.leo009@gmail.com>
2024-05-11 23:36:04 -0400

    Lecture 47 - Parent to child & back to parent & again to child's sibling
    1. Parent to child and traversing back to parent and then again to
    child's sibling - i.e., to log in button
    2. //header/div/button[1]/parent::div/button[2]

--- src/Locators3.java
+++ src/Locators3.java
@@ -20,6 +20,10 @@ public class Locators3
 		String logInButton = driver.findElement(By.xpath("//header/div/button[1]/following-sibling::button[1]")).getText();
 		System.out.println(logInButton);
 		
+		//Parent to child and traversing back to parent and then again to child's sibling - i.e., to log in button
+		String logInButton1 = driver.findElement(By.xpath("//header/div/button[1]/parent::div/button[2]")).getText();
+		System.out.println(logInButton1);
+		
 		Thread.sleep(2000);
 		
 		//close
