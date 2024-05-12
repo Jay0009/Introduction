7e30dfe95be3a956acda0737ae220273976e4274
Jay <jay.leo009@gmail.com>
2024-05-12 14:38:01 -0400

    Lecture 63,64, check if one way is selected by get attribute IMP
    1. Check if 1 way is selected id - ctl00_mainContent_rbtnl_Trip_0
    2. Click on round trip, id - ctl00_mainContent_rbtnl_Trip_1
    3. Select current date by css .class, remember to remove any space if
    class name is combination of 2 class, class of current date will be
    unique .ui-state-default.ui-state-highlight
    4. Check if the return day section is enabled or disabled, but isEnable
    will not work as if we click on return day, it will be highlighted and
    the radio button will automatically shift to round trip as the website
    is designed dynamically.
    5. To resolve this, in round trip, check the html code and take a
    screenshot, now select one way and and take another screenshot of the
    html code for that div/section, one attribute will be changed. Here in
    this case, it is opacity which changed from 1 to 0.5 in style attribute

--- src/automation/DynamicDropdown.java
+++ src/automation/DynamicDropdown.java
@@ -3,6 +3,7 @@ package automation;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
 
 public class DynamicDropdown
 {
@@ -15,6 +16,43 @@ public class DynamicDropdown
 		
 		//get URL
 		driver.get("https://rahulshettyacademy.com/dropdownsPractise/");
+		
+		//Check if 1 way is selected
+		boolean oneWaySelected = driver.findElement(By.id("ctl00_mainContent_rbtnl_Trip_0")).isSelected();
+		System.out.println(oneWaySelected);
+		
+		//Check if return day section is highlighted in one way
+		if (driver.findElement(By.id("Div1")).getAttribute("style").contains("0.5"))
+		{
+			System.out.println("One way is selected and return date is not enabled");
+			Assert.assertFalse(false);
+		}
+		else 
+		{
+			Assert.assertFalse(true);
+		}
+		
+		Thread.sleep(5000);
+		
+		//Click on round trip
+		driver.findElement(By.id("ctl00_mainContent_rbtnl_Trip_1")).click();
+		
+		Thread.sleep(2000);
+		
+		//Check if round trip is selected
+		boolean roundTripSelected = driver.findElement(By.id("ctl00_mainContent_rbtnl_Trip_1")).isSelected();
+		System.out.println(roundTripSelected);
+		
+		//Check if return day section is highlighted in round trip way
+		if (driver.findElement(By.id("Div1")).getAttribute("style").contains("1"))
+		{
+			System.out.println("Round trip is selected and return date is enabled");
+			Assert.assertTrue(true);
+		}
+		else 
+		{
+			Assert.assertTrue(false);
+		}
 		
 		//Click on From (city
 		driver.findElement(By.id("ctl00_mainContent_ddl_originStation1_CTXT")).click();
@@ -29,6 +67,12 @@ public class DynamicDropdown
 		
 		//Select to/destination city - without index with parent child relationship xpath
 		driver.findElement(By.xpath("//div[@id='glsctl00_mainContent_ddl_destinationStation1_CTNR'] //a[@value='MAA']")).click();
+		
+		Thread.sleep(2000); // to let window appear
+		
+		// Select current date by css .class, remember to remove any space if class name is combination of 2 class, 
+		//class of current date will be unique
+		driver.findElement(By.cssSelector(".ui-state-default.ui-state-highlight")).click();
 		
 		Thread.sleep(2000);
 		driver.close();
