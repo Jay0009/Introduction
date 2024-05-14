b18d262bad654d114fa98d170623292530f56968
Jay <jay.leo009@gmail.com>
2024-05-14 15:58:49 -0400

    Section 9 - Lecture 79-80 - implicit wait

--- src/ecommerce/Base.java
+++ src/ecommerce/Base.java
@@ -1,7 +1,9 @@
 package ecommerce;
 
+import java.time.Duration;
 import java.util.Arrays;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
@@ -17,18 +19,43 @@ public class Base
 		System.setProperty("webdriver.chome.driver", "/Users/jay/Desktop/VdemiCourse/driver/chrome-mac-arm64/Google Chrome for Testing.app");
 		WebDriver driver = new ChromeDriver();
 		
+		//9. implicit wait
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+		
+		
 		//1. Declare itemsNeeded
 		String[] itemsNeeded  = {"Cucumber", "Brocolli", "Beetroot"};
 
 		//2. URL
 		driver.get("https://rahulshettyacademy.com/seleniumPractise/");
 		
+		//3. add items
 		addItems(driver, itemsNeeded);
+		
+		//4. click on cart
+		driver.findElement(By.cssSelector("img[alt='Cart']")).click();
+		
+		//5. Proceed to checkout
+		driver.findElement(By.xpath("//button[text()='PROCEED TO CHECKOUT']")).click();
+		
+		//6. input promo code
+		driver.findElement(By.cssSelector("input.promoCode")).sendKeys("rahulshettyacademy");
+		
+		//7. click on promo button
+		driver.findElement(By.cssSelector("button.promoBtn")).click();
+		
+		//8. check if promo aplied message is visible - please note that promo info visible after 
+		// few seconds.
+		String promoMessage = driver.findElement(By.cssSelector("span.promoInfo")).getText();
+		System.out.println(promoMessage);
+		
+		
+		
 		Thread.sleep(3000);
 		driver.close();
 		
+		
 	}
-	
 	
 	public static void addItems(WebDriver driver, String[] itemsNeeded)
 	{
