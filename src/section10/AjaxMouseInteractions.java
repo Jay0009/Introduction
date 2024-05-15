5d1451a4828c824d29079b991a398c90afefaeb0
Jay <jay.leo009@gmail.com>
2024-05-15 03:32:17 -0400

    update to firefox as chrome shows a captcha code

--- src/section10/AjaxMouseInteractions.java
+++ src/section10/AjaxMouseInteractions.java
@@ -2,7 +2,7 @@ package section10;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.interactions.Actions;
 
 public class AjaxMouseInteractions
@@ -11,13 +11,12 @@ public class AjaxMouseInteractions
 	public static void main(String[] args) throws InterruptedException
 	{
 		//1. Configure driver
-		System.setProperty("webdriver.chome.driver", "/Users/jay/Desktop/VdemiCourse/driver/chrome-mac-arm64/Google Chrome for Testing.app");
-		WebDriver driver = new ChromeDriver();
+		System.setProperty("webdriver.gecko.driver", "//Users/jay/Desktop/VdemiCourse/driver/geckodriver");
+		WebDriver driver = new FirefoxDriver();
 		
 		//2. URL
-		driver.get("https://www.amazon.com/");
+		driver.get("https://www.amazon.ca/");
 		
-		Thread.sleep(7000);
 		
 		//3. Move mouse to section for the menu to pop  up
 		Actions a = new Actions(driver);
