cc4f76472a8cdc3895386f45a89c005338aa10e4
Jay <jay.leo009@gmail.com>
2024-05-16 03:45:06 -0400

    l -99-100 - select date & match it with given date.

--- src/section11/calenderTest.java
+++ src/section11/calenderTest.java
@@ -1,8 +1,12 @@
 package section11;
 
+import java.util.List;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
 
 public class calenderTest
 {
@@ -17,6 +21,7 @@ public class calenderTest
 		String month = "6";
 		String year = "2027";
 		String day = "15";
+		String[] expectedDate = {month, day, year};
 		
 		//3. URL
 		driver.get("https://rahulshettyacademy.com/seleniumPractise/#/offers");
@@ -38,6 +43,21 @@ public class calenderTest
 		
 		//9 click on day
 		driver.findElement(By.cssSelector("abbr[aria-label*='"+day+"']")).click();
+		
+		//10. date selected - this will not work because, the date 6/15/2025 is individually placed in a subclass
+//		String dateSelected = driver.findElement(By.cssSelector(".react-date-picker__inputGroup")).getText();
+//		System.out.println(dateSelected);
+		
+		//11. As above method in step 10 will not work, we'd have to select individual sub class
+		List<WebElement> actualDate = driver.findElements(By.cssSelector(".react-date-picker__inputGroup__input"));
+		
+		for (int i = 0; i < actualDate.size(); i++)
+		{
+			System.out.println(actualDate.get(i).getAttribute("value"));
+			Assert.assertEquals(actualDate.get(i).getAttribute("value"), expectedDate[i]);
+		}
+		
+		
 		
 		Thread.sleep(3000);
 		driver.close();
