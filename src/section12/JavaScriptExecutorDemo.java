44eb95898a1de33bd6da3fa16e52fe1d2030c25d
Jay <jay.leo009@gmail.com>
2024-05-16 05:05:17 -0400

    L-102-103 - sum all the values in the table object
    1. url https://rahulshettyacademy.com/AutomationPractice/
    2. sum all the values from table.
    3. compare actual and expected sum.

--- src/section12/JavaScriptExecutorDemo.java
+++ src/section12/JavaScriptExecutorDemo.java
@@ -1,8 +1,13 @@
 package section12;
 
+import java.util.List;
+
+import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
 
 public class JavaScriptExecutorDemo
 {
@@ -26,6 +31,25 @@ public class JavaScriptExecutorDemo
 		
 		//5.scroll on web element to go to bottom
 		js.executeScript("document.querySelector('.tableFixHead').scrollTop=500");
+		
+		//6. sum all values
+		List<WebElement> values = driver.findElements(By.cssSelector(".tableFixHead td:nth-child(4)"));
+		
+		//7.sum all values
+		int actualSum = 0;
+		for (int i = 0; i < values.size(); i++)
+		{
+			actualSum += Integer.parseInt(values.get(i).getText());
+		}
+		System.out.println(actualSum);
+		
+		//8. get total amount mentioned/expectedTotal
+		String[] totalAmountString = driver.findElement(By.cssSelector(".totalAmount")).getText().split(" ");
+		String expectedTotalString = totalAmountString[totalAmountString.length-1];
+		int expectedSum = Integer.parseInt(expectedTotalString);
+		
+		//9. compare actual and expected sum
+		Assert.assertEquals(expectedSum, actualSum);
 		
 		Thread.sleep(3000);
 		driver.close();
