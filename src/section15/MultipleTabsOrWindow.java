d0dab21989dff688c86cba3187799de0821efddd
Jay <jay.leo009@gmail.com>
2024-05-20 15:43:54 -0400

    S.15.126- take the screenshot of the webselemt - nameEditBox
    1.take the screenshot of the webElement namEditBox in which the first
    course name was pasted earlier
    File file = nameEditbox.getScreenshotAs(OutputType.FILE);
    FileUtils.copyFile(file, new File("nameEditbox.png"));

--- src/section15/MultipleTabsOrWindow.java
+++ src/section15/MultipleTabsOrWindow.java
@@ -1,17 +1,22 @@
 package section15;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.Iterator;
 import java.util.Set;
 
+import org.apache.commons.io.FileUtils;
 import org.openqa.selenium.By;
+import org.openqa.selenium.OutputType;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.WindowType;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class MultipleTabsOrWindow
 {
 
-	public static void main(String[] args) throws InterruptedException
+	public static void main(String[] args) throws InterruptedException, IOException
 	{
 		// 1. Configure driver
 		System.setProperty("webdriver.chome.driver",
@@ -33,7 +38,16 @@ public class MultipleTabsOrWindow
 		driver.get("https://rahulshettyacademy.com/");
 		String firstCourseName = driver.findElements(By.cssSelector("a[href *= 'courses.rahulshettyacademy.com/p']")).get(1).getText();
 		driver.switchTo().window(parentWindow);
-		driver.findElement(By.name("name")).sendKeys(firstCourseName);
+		WebElement nameEditbox = driver.findElement(By.name("name"));
+		nameEditbox.sendKeys(firstCourseName);
+		
+		//Lecture 126. - 1.take the screenshot of the webElement namEditBox 
+		//in which the first course name was pasted earlier
+		File file = nameEditbox.getScreenshotAs(OutputType.FILE);
+		FileUtils.copyFile(file, new File("nameEditbox.png"));
+		
+		
+		
 		Thread.sleep(2000);
 		driver.quit();
 		
